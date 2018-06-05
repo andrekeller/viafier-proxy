10542dce45ac8be002f158dd44bc252a9cbf3e05
André Keller <andre.keller@vshn.ch>
2018-06-05 22:25:07 +0200

    Make caching configurable

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 if [[ $1 = 'nginx' ]]; then
-    erb /etc/nginx/nginx.conf.erb > /run/nginx/nginx.conf
+    erb -T- /etc/nginx/nginx.conf.erb > /run/nginx/nginx.conf
     nginx -c /run/nginx/nginx.conf
 else
     exec "$@"
